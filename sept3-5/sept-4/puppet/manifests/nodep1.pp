7262c2f2e8d1accd4606841f11843aa80b96f99c
Aditya <sp.aditya@gmail.com>
2018-09-05 10:19:34 +0530

    Update nodep1.pp

--- sept3-5/sept-4/puppet/manifests/nodep1.pp
+++ sept3-5/sept-4/puppet/manifests/nodep1.pp
@@ -9,15 +9,6 @@ node /nodep[1-9]/{
   	File['/etc/apache2/ports.conf'] ~> Service['apache2']
   }
   if $::boxrole == 'db' {
-	$override_options = {
-  		'mysqld' => {
-    		'bind-address' => '0.0.0.0',
-  		}
-	}
-	class { '::mysql::server':
- 		 root_password           => 'example',
-  		remove_default_accounts => true,
-  		override_options        => $override_options
-	}
+	class { '::mysql::server':}
   }
 }
